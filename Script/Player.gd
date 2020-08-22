a5813d785845edbd5775fb171fabef45b7c53415
Harmony Monroe <HarmonyMonroe97@gmail.com>
2020-08-22 19:51:15 -0400

    walk n jump

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,9 +1,30 @@
 extends Actor
 
+var move_speed = 1
+var move_slow = 0.75
+var move_accel = 0.15
+var move_last = 0
+var push_speed = 0.3
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var jump_speed = 2
+var jump_frames = 10
+var jump_count = 0
+var is_jump = false
+
+var anim_frame = 0
+var anim_step_run = 0.2
+var anim_step_idle = 0.04
+
+var is_holding_crate = false
+
+var speed_drop_x = 1
+var speed_drop_y = -1
+
+var speed_throw_x = 1.0
+var speed_throw_y = -2.3
+
+var pickup_frames = 8
+var pickup_count = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,12 +40,33 @@ func _process(delta):
 	var btnx = btn.d("right") - btn.d("left")
 	var btny = btn.d("down") - btn.d("up")
 	
-	speed_x = btnx * 0.5
-	#speed_y = btny * 3
 	
-	if btn.p("jump") and is_on_floor:
-		speed_y = -3
+	# walking
+	if btnx == 0:
+		speed_x *= move_slow
+	else:
+		speed_x += move_accel * btnx
+		#speed_x = mid(-move_speed, speed_x, move_speed)
+		speed_x = clamp(speed_x, -move_speed, move_speed)
+		#flip_x = btn(0)
 	
+	# start jump
+	if btn.p("jump") and check_jump():
+		is_jump = true
+		jump_count = 0
 	
+	# jump height
+	if is_jump:
+		if btn.d("jump"):
+			speed_y = -jump_speed
+			jump_count += 1
+			if jump_count > jump_frames:
+				is_jump = false
+		else:
+			is_jump = false
 
+
+
+func check_jump():
+	return is_on_floor or is_on_floor_2 or is_on_floor_3
 
