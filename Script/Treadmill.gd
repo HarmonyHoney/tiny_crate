5f476210ff3fea90869cc1febccd0e4c41e25a78
Harmony Monroe <HarmonyMonroe97@gmail.com>
2020-10-05 13:26:03 -0400

    scrolling shader for treadmill + setget tools adjusting tread_speed will show results in editor changing length in editor will affect sprite and hitbox_x changing editor_move will move the actor that many tiles

--- Script/Treadmill.gd
+++ Script/Treadmill.gd
@@ -3,16 +3,22 @@ extends Actor
 class_name Treadmill
 
 export var tread_speed := -1.0 setget _set_tread_speed
+export var length := 1 setget _set_length
+export var editor_move := 0 setget _set_editor_move
 
 var node_sprite : Sprite
 
 
 func _ready():
-	
+	# editor and game code
 	node_sprite = $Sprite
+	_set_tread_speed(tread_speed)
+	_set_length(length)
 	
 	if Engine.editor_hint:
 		return
+	
+	# game only code
 
 func _process(delta):
 	if Engine.editor_hint:
@@ -21,7 +27,15 @@ func _process(delta):
 
 func _set_tread_speed(value):
 	tread_speed = value
-	
 	if node_sprite:
 		node_sprite.flip_h = tread_speed > 0
-	
+		node_sprite.material.set_shader_param("speed", abs(tread_speed * 8))
+
+func _set_length(value):
+	length = max(1, value)
+	_set_hit_x(8 * length)
+	if node_sprite:
+		node_sprite.region_rect = Rect2(Vector2.ZERO, Vector2(8 * length, 8))
+
+func _set_editor_move(value):
+	position.x += value * 8
---
+++ Shader/scrollx.shader
@@ -0,0 +1,7 @@
+shader_type canvas_item;
+render_mode blend_mix;
+
+uniform float speed = 1.0;
+void fragment() {
+    COLOR = texture(TEXTURE, UV + vec2(TIME * speed, 0.0));
+}
