faeb668f210e41e31a40020dd38b5f3fc4656224
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-08-26 17:39:09 -0400

    include export_presets.cfg and butler_push.bat (;

---
+++ butler_push.bat
@@ -0,0 +1,5 @@
+cd export
+butler push win harmonyhoney/tinycrate:windows
+butler push linux harmonyhoney/tinycrate:linux
+butler push mac harmonyhoney/tinycrate:mac
+butler push web harmonyhoney/tinycrate:web
