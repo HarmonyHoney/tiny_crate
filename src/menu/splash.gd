3865b90f8020635694ae41ecb82ed0c4dff4dccd
Harmony Honey <HarmonyMonroe97@gmail.com>
2022-12-02 20:07:13 -0500

    new splash! (;

--- src/menu/splash.gd
+++ src/menu/splash.gd
@@ -1,22 +1,8 @@
 extends Node2D
 
-var timer = 0.7
-var step = 0
-onready var logo = $Logo
-onready var audio = $AudioStreamPlayer
-
 func _ready():
-	logo.visible = false
+	yield(get_tree(), "idle_frame")
 	Music.play()
-
-func _physics_process(delta):
-	timer = max(0, timer - delta)
-	if timer == 0:
-		if step == 0:
-			logo.visible = true
-			audio.play()
-			step = 1
-			timer = 1.5
-		else:
-			Shared.wipe_scene(Shared.main_menu_path)
-			set_physics_process(false)
+	$AudioStreamPlayer.play()
+	yield(get_tree().create_timer(1.5), "timeout")
+	Shared.wipe_scene(Shared.main_menu_path)
