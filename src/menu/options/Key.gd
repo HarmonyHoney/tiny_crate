49f6a2e58d5499a3784794bc7e5087e1a73205c2
Harmony Honey <HarmonyMonroe97@gmail.com>
2024-01-19 21:57:14 -0500

    testing dynamic keys (:

--- src/menu/options/Key.gd
+++ src/menu/options/Key.gd
@@ -1,14 +1,38 @@
 tool
 extends Control
 
+export var action := "" setget set_action
 export var text := "key" setget set_text
 
 onready var white := $White
 onready var black := $Black
 onready var label := $Label
 
+var is_gamepad := false
+
+var swap := {"control" : "ctrl",
+"quoteleft" : "~",
+"capslock" : "caps",
+"escape" : "esc",
+"equal" : "=",
+"minus" : "-",
+"bracketleft" : "[",
+"bracketright" : "]",
+"apostrophe" : "'",
+"semicolon" : ";",
+"comma" : ",",
+"period" : ".",
+"slash" : "/",
+"backslash" : "\\",
+"insert" : "ins",
+"delete" : "del",
+"pageup" : "pgup",
+"pagedown" : "pgdn",
+}
+
+
 func _ready():
-	set_text()
+	set_action()
 	if Engine.editor_hint: return
 
 func set_text(arg := text):
@@ -26,4 +50,37 @@ func set_text(arg := text):
 	if is_instance_valid(black):
 		black.rect_size = Vector2(l + 1, 7)
 	
+	update()
+
+func set_action(arg := action):
+	action = arg
+	print("set_action = ", action, " ", InputMap.get_action_list(action))
+	
+	
+	if action != "" and InputMap.has_action(action):
+		var l = InputMap.get_action_list(action)
+		print("l")
+		var e = null
+		
+		for i in l:
+			if is_type(i):
+				e = i
+		
+		if e:
+			parse_event(e)
+
+func is_type(event):
+	var test = !is_gamepad and event is InputEventKey
+	if !test:
+		test = is_gamepad and (event is InputEventJoypadButton or event is InputEventJoypadMotion)
+	
+	return test
+
+func parse_event(event : InputEvent):
+	var s = str(event.as_text().to_lower())
+	
+	if swap.has(s):
+		s = swap[s]
+	
+	self.text = s
 	update()
