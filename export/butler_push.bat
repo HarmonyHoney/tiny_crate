0c7cc46644084b5a458240e4581902641557ea70
Harmony Honey Monroe <harmonymonroe@proton.me>
2025-01-16 13:42:30 -0500

    remove butler_push

--- export/butler_push.bat
+++
@@ -1,6 +0,0 @@
-butler push win harmonyhoney/tinycrate:windows
-butler push linux harmonyhoney/tinycrate:linux
-butler push mac harmonyhoney/tinycrate:mac
-butler push web harmonyhoney/tinycrate:web
-butler push android harmonyhoney/tinycrate:android
-pause
--- export/butler_push.sh
+++
@@ -1,7 +0,0 @@
-export PATH=/home/harmony/apps/butler-linux-amd64:$PATH
-butler push win harmonyhoney/tinycrate:windows
-butler push linux harmonyhoney/tinycrate:linux
-butler push mac harmonyhoney/tinycrate:mac
-butler push web harmonyhoney/tinycrate:web
-butler push android harmonyhoney/tinycrate:android
-bash
