1875ba7046b553a6ab2c9564258a876bbb56536f
Harmony Honey Monroe <harmonymonroe@proton.me>
2025-02-18 00:22:22 -0500

    buter_push.bat and .sh ! splash image tweak

---
+++ export/butler_push.bat
@@ -0,0 +1,6 @@
+butler push win harmonyhoney/tinycrate-open:windows
+butler push linux harmonyhoney/tinycrate-open:linux
+butler push mac harmonyhoney/tinycrate-open:mac
+butler push web harmonyhoney/tinycrate-open:web
+butler push android harmonyhoney/tinycrate-open:android
+pause
---
+++ export/butler_push.sh
@@ -0,0 +1,7 @@
+export PATH=/home/harmony/apps/butler-linux-amd64:$PATH
+butler push win harmonyhoney/tinycrate-open:windows
+butler push linux harmonyhoney/tinycrate-open:linux
+butler push mac harmonyhoney/tinycrate-open:mac
+butler push web harmonyhoney/tinycrate-open:web
+butler push android harmonyhoney/tinycrate-open:android
+bash
